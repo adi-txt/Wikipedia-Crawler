cf9a1e870ba0f3e092849b3a1aeb0f2f30d8aa40
adi-txt <adithya.met92@gmail.com>
2018-01-18 09:52:49 -0500

    readability updates

--- wiki-crawler.py
+++ wiki-crawler.py
@@ -24,13 +24,13 @@ def find_first_link(url):
 	return first_link
 
 # This function checks if our Wikipedia crawler has hit the target url, crossed the max number of steps, or has hit a loop.
-def continue_crawl(search_history, target_url, max_steps = 25):
+def continue_crawl(search_history, target_url, max_links = 25):
     if search_history[-1] == target_url:													
         print("\nWe've reached your specified target url - " + target_url[25:] + "\n")
         print("Wiki crawler ending...")
         print("Wiki crawler ended.\n")
         return False
-    elif len(search_history) > max_steps:
+    elif len(search_history) > max_links:
         print("\nOur search history has hit the 25 article barrier.\n")
         print("Wiki crawler ending...")
         print("Wiki crawler ended.\n")
@@ -46,10 +46,10 @@ def continue_crawl(search_history, target_url, max_steps = 25):
 
 # This is where the program begins running
 
-start_url = "https://en.wikipedia.org/wiki/Special:Random" 				# leads to a random Wikipedia page
+start_url = "https://en.wikipedia.org/wiki/Special:Random" # leads to a random Wikipedia page
 target_url = "https://en.wikipedia.org/wiki/Language"
-article_chain = [start_url] 											# the list of articles crawled
-article_count = 1 														# the number of articles crawled.
+article_chain = [start_url] # the list of articles crawled
+article_count = 1 # the number of articles crawled.
 
 
 print("\nYour start url is: " + start_url)
@@ -68,7 +68,7 @@ while continue_crawl(article_chain, target_url):
 		print("Wiki crawler ended.\n")
 		break
 	
-	article_chain.append(first_link)									# add the first link found to article_chain
+	article_chain.append(first_link) # add the first link found to article_chain
 	
-	article_count += 1 													# increment the article count
-	time.sleep(2)														# delay for about two seconds
+	article_count += 1 # increment the article count
+	time.sleep(2) # delay for about two seconds
